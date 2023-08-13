644df042940d634a9b98abed6ae144d1bbd9c64a
tatyana <tilkka@mail.ru>
2023-08-13 20:48:53 +0300

    fix: logic to work with digits

--- src/index.js
+++ src/index.js
@@ -1,49 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-    const temp = [].concat(...bracketsConfig).join('').split(''); // массив скобок из bracketsConfig
-    
-    let counter = 0;
-    const strToArr = str.split(''); // массив скобок str
-  
-    temp.forEach(element => {
-        if (strToArr.indexOf(element) !== -1) {
-            counter++;
-        }
-    });
-
-    if (temp.length !== counter) { // проверка, что в обоих частях есть одинаковые элементы
-        return false;
-    }
-    if (str === temp.join('')) { // если явно совпадают, выходим
-        return true;
-    }
-  
-    counter = 0;
-    const openBracketsAndDigits = ['(', '[', '{'];
-    const closedracketsAndDigits = [')', ']', '}', '|', '1', '2', '3', '4', '5', '6', '7', '8'];
-    const sequence = ['[]', '{}', '()', '||'];
+    const config = bracketsConfig.map(pairOfbrackets => pairOfbrackets.join('')); 
+    const arrayOfBracketsToCheck = str.split('');
+    const openBracketsAndDigits = ['(', '[', '{', '1', '3', '5'];
+    const closedBracketsAndDigits = [')', ']', '}', '|', '2', '4', '6', '7', '8'];
     
     let resultArray = [];
  
-    strToArr.forEach(symbol => {
-        if (counter < 0) { // если счетчик не обнулился, то значит количество открывающихся и закрывающихся скобок не равны
-            return false;
-        }
+    arrayOfBracketsToCheck.forEach(symbol => {
         if (openBracketsAndDigits.includes(symbol)) {   
-            counter++;
-            resultArray.push(symbol); 
-        } 
-        if (closedracketsAndDigits.includes(symbol)) {
-            counter--;
             resultArray.push(symbol);
-            if (sequence.includes(resultArray.slice(-2).join(''))) { // проверяем если в конце есть открытая и закртиая скобка одинаковая, то удаляем ее
+        } 
+        if (closedBracketsAndDigits.includes(symbol)) {
+            resultArray.push(symbol);
+            if (config.includes(resultArray.slice(-2).join(''))) {
                 resultArray.splice(resultArray.length - 2, 2);
-            }
-            if (symbol === '|') { // этот символ всем мешает :)
-                counter++;
             }
         }
     });
- 
-    return (counter === 0 && resultArray.length === 0); // если кол-во скобок одинаковое или в массиве не осталось скобок, значит совпадение есть, иначе ложь
+    return (resultArray.length === 0); 
 }
 
